58306c86649ea0e113a88ab04f0e9638b2a26cc8
gilc1870 <gilc1870@wrdsb.ca>
2020-10-14 17:15:38 +0000

    Helps get the total of the pizza

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+
+
+ * This will determine the price of a pizza
+ * @author Cesar Gil
  */
 public class Main {
 
@@ -9,7 +13,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Scanner for the pizza cm
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("What is the size of the pizza?(In cm)");
+    int size = input.nextInt();
+  
+  
+   // Calculates and shows total price
+  System.out.println("Subtotal:" + (0.75 + 0.99 + 0.50 * size));
+  System.out.println("Taxes:" + 0.13 * (0.75 + 0.99 + 0.50 * size));
+  System.out.println("Total:" + 1.13 * ( 0.75 + 0.99 + 0.50 * size));
+
+  // reponse   
+   if (size > 40){ 
+    System.out.println("Whoa, big pizza! You might need a truck to get this home!");
+   } else if (size > 20){
+     System.out.println("This will be delicious!");
+   } else if (size > 0){
+     System.out.println("We are going to make you a cute little pizza!");
+   }
   }
 }
